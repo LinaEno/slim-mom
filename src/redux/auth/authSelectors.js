f2ef57d95b1010334ff27a70031aa3fe5f00f979
Lina Budkova <linabudkova35@gmail.com>
2023-03-24 15:36:27 +0200

    Update authSelectors.js

--- src/redux/auth/authSelectors.js
+++ src/redux/auth/authSelectors.js
@@ -2,10 +2,6 @@ export const selectIsLoggedIn = state => state.auth.isLoggedIn;
 
 export const getUserName = state => state.auth.userName;
 
-export const userData = state => state.auth.user;
-
-export const selectUserBalance = state => userData(state).balance;
-
 export const selectRefreshed = state => state.auth.isRefreshing;
 
 export const selectToken = state => state.auth.token;
