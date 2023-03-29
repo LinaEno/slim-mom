ff16877bd470a0706d28fd9da882548c9f07d9aa
Lina Budkova <linabudkova35@gmail.com>
2023-03-29 21:04:07 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,7 +7,6 @@ import { PersistGate } from 'redux-persist/integration/react';
 import { store, persistor } from 'redux/store';
 import { BrowserRouter } from 'react-router-dom';
 
-import './utils/i18n';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
